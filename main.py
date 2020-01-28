dfdd6161e5a752e109a4566e0fcb43b307e31b40
Jdude311 <jadenjohn.ch@gmail.com>
2020-01-28 12:41:18 -0500

    Added main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+from math import sqrt
+print "Hello World!"
+print "This is the prime number checker!"
+
+start_number = input("What number would you like to start at? (It has to be greater than 0)  ")
+end_number = input("What number would you like to end at? ")
+counter = start_number
+
+def is_divisible(dividend, divisor):
+    #print "dividend % divisor" + str(dividend % divisor)
+    if (dividend % divisor == 0):
+      divisible = True
+    else:
+      divisible = False
+    return divisible
+
+primes = []
+
+while counter <= end_number:
+  top = sqrt(counter)
+  #print top
+
+  i = 2
+
+  
+
+  div = False
+
+  while (i <= top and div == False):
+    div = is_divisible(counter, i)
+    i += 1
+    #print div
+    #print i
+    #print 
+
+  #print div
+  if div == False:
+    print( str(counter) + " is prime yoooooo!")
+    primes.append(counter)
+  else:
+    print( str(counter) + " is not prime. Let's get an F in the chat bro")
+  counter += 1
+
+for prime in primes:
+  print(prime)
